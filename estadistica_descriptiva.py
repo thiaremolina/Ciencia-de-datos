fb94dd88843357cc9f02c685bd3eb2d365969a42
Thiare <thiare.molina@usach.cl>
2026-01-05 16:34:43 -0300

    archivo final
    Cambie el formato del archivo y corregí algunos errores que fui descubriendo a medida que hacia la tarea.

--- estadistica_descriptiva.py
+++ estadistica_descriptiva.py
@@ -36,12 +36,20 @@ def mediana(vals):
     mediana: float
           la mediana de los números
     """
+    # 1. Revisar valores Nan:
+    vals = [x for x in vals if not math.isnan(x)] 
+    if not vals:
+        return float('Nan')
+
+    #  2. Ordenar los valores y calcular n(el largo de la lista):
     lista_ordenada = sorted(vals)
     n = len(lista_ordenada)
+
+    # 3. Si el número de datos es par, promediamos los dos valores centrales:
     if n % 2 == 0:
-        # Si el número de datos es par, promediamos los dos valores centrales:
-        mid1 = lista_ordenada(n // 2 - 1)
-        mid2 = lista_ordenada(n // 2)
+        mid1 = lista_ordenada[n // 2 - 1] 
+        # pq []? porque los [] acceden a los elemento de unna lista, mientras que los () llaman a una función.
+        mid2 = lista_ordenada[n // 2]
         return (mid1 + mid2) / 2
     else:
         # Si es impar, el valor central es la mediana:
@@ -61,8 +69,9 @@ def moda(vals):
     moda: int
           la moda de los números
     """
+        
     # 1. determinar las categorias 
-    categorias = [] 
+    categorias = []
     for i in vals: 
         if i not in categorias:
             categorias.append(i)
@@ -132,20 +141,25 @@ def varianza(vals, tipo ='Poblacional'):
          la varianza de los números
           
     """
-     # 1. condiciones iniciales:
+    # 1. Revisar valores Nan:
+     vals = [x for x in vals if not math.isnan(x)] 
+     if not vals:
+        return float('Nan')
+        
+    # 2. condiciones iniciales:
      n = len(vals)
      if n < 2:
          return 0
      media = promedio(vals)
-     # 2. calcular la sumatoria de las  diferencias al cuadrado:
+     # 3. calcular la sumatoria de las  diferencias al cuadrado:
      diferencias_cuadrado = sum([(x - media) ** 2 for x in vals])
-     # 3. Determinar el denominador según el tipo:
+     # 4. Determinar el denominador según el tipo:
      if tipo =='Muestral': 
-         denominador = n-1 
+         denominador =  n-1 
      else:
          denominador = n
-     # 4. Calcular la varianza:
-     varianza = diferencias_cuadrado/denominador
+         varianza = diferencias_cuadrado/denominador
+     return varianza
 
      
 def desviacion_estandar(vals):  
@@ -161,8 +175,14 @@ def desviacion_estandar(vals):
     desviación estándar: float
          la desviación estándar de los números
           
-    """ 
-    desviacion_estandar = math.sqrt(varianza)
+    """
+    # 1. Revisar valores Nan:
+    vals = [x for x in vals if not math.isnan(x)] 
+    if not vals:
+        return float('Nan')
+    
+    v = varianza(vals, tipo='Poblacional')
+    desviacion_estandar = math.sqrt(v)
     return desviacion_estandar
 
 def desviacion_mediana_absoluta(vals):  
@@ -178,14 +198,20 @@ def desviacion_mediana_absoluta(vals):
     desviación mediana aboluta: float
          la desviacion mediana absoluta de la mediana de los números
           
-    """ 
-    mediana = mediana(vals)
-    desviaciones = [abs(x - mediana) for x in vals] #abs() devuelve el valor absoluto de un número
+    """
+    # 1. Revisar valores Nan:
+    vals = [x for x in vals if not math.isnan(x)] 
+    if not vals:
+        return float('Nan')
+        
+    med = mediana(vals)
+    desviaciones = [abs(x - med) for x in vals] #abs() devuelve el valor absoluto de un número
     return mediana(desviaciones)
 
 def percentil(vals,percentil):
     """
     Calcula el percentil de una lista de números.
+    Verifica y elimina NaNs en los datos.
     
     Parametros
     ---------------
@@ -200,22 +226,27 @@ def percentil(vals,percentil):
          percentil solicitado de una lista de números
           
     """
-    # 1. ordenar los datos:
+    # 1. Revisar valores Nan:
+    vals = [x for x in vals if not math.isnan(x)] 
+    if not vals:
+        return float('Nan')
+        
+    # 2. ordenar los datos:
     lista = sorted(vals)
     n = len(lista)
-    # 2. encontrar la posicion:
+    # 3. encontrar la posicion:
     posicion = (percentil*(n-1) /100)
-    # 3. revisar si la posicion es exacta o decimal:
+    # 4. revisar si la posicion es exacta o decimal:
     parte_entera = int(posicion)
     parte_decimal = posicion - parte_entera
     if parte_decimal == 0:
         percentil = lista[parte_entera]
-    # 4. calcular el valor antecesor, sucesor y su distancia
+    # 5. calcular el valor antecesor, sucesor y su distancia
     else:
         primer_valor = lista[parte_entera]
         segundo_valor = lista[parte_entera+1]
         distancia = segundo_valor - primer_valor
-        # 5. calcular el percentil
+        # 6. calcular el percentil
         percentil = primer_valor + (parte_decimal * distancia)
 
     return percentil
@@ -238,6 +269,7 @@ def rango_intercuartilico(vals):
     q1 = percentil(vals, 25)
     q3 = percentil(vals, 75)
     rango_intercuartilico = q3 - q1
+    return rango_intercuartilico
 
 def covarianza(vals_x,vals_y):
     """
@@ -253,18 +285,22 @@ def covarianza(vals_x,vals_y):
     covarianza: float
           covarianza de los atributos
     """
-    # 1. condiciones:
+    # 1. Revisar los valores Nan:
+    vals_x = [x for x in vals_x if not math.isnan(x)]
+    vals_y = [x for x in vals_y if not math.isnan(x)]
+    
+    # 2. Condiciones:
     n = len(vals_x)
     if len(vals_y) != n:
         return 'Las listas tienen que ser de la misma longuitud'
     if n < 2:
         return 0
     
-    # 2. Calcular las medias:
+    # 3. Calcular las medias:
     m1 = promedio(vals_x)
     m2 = promedio(vals_y)
 
-     # 3. Calcular la suma de los productos de las diferencias:
+     # 4. Calcular la suma de los productos de las diferencias:
     sumatoria = 0 
     for i in range(n):
         dif_x = vals_x[i] - m1
@@ -289,9 +325,18 @@ def correlacion (vals_x,vals_y):
     correlacion: float
           correlacion de los atributos
     """
+    # 1. Revisar valores Nan:
+    vals_x = [x for x in vals_x if not math.isnan(x)]
+    vals_y = [x for x in vals_y if not math.isnan(x)]
+
+    # 2.Calcular desviacion estandar de x e y:
     desv_x = desviacion_estandar(vals_x)
     desv_y = desviacion_estandar(vals_y)
+
+    # 3. calcular correlacion:
     correlacion = covarianza(vals_x, vals_y) / (desv_x * desv_y)
+
+    # 4. en caso de que la desviacion sea cero:
     if desv_x == 0 or desv_y == 0:
         return None
     else:
